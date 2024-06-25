92e72e38c8e05c17e529b77a4458d2ce5736b573
ghinafitri04 <ghinafitri605@gmail.cpm>
2024-06-25 17:22:48 +0700

    link file

--- models/mou.js
+++ models/mou.js
@@ -3,52 +3,52 @@ import sequelize from "../config/db.js";
 import customer from "./customer.js";
 
 const mou = sequelize.define(
-    "mou",
-    {
-        id: {
-            type: Sequelize.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-            allowNull: false,
-        },
-        start_date: {
-            type: Sequelize.DATE,
-            allowNull: false,
-        },
-        finish_date: {
-            type: Sequelize.DATE,
-            allowNull: false,
-        },
-        mou_file: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        customer_id: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-        },
+  "mou",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
     },
-    {
-        tableName: "mou",
-        underscored: true,
-        timestamps: true,
-        getterMethods: {
-            file_url() {
-                return `http://localhost:3000/${this.mou_file}`;
-            },
-        },
-    }
+    start_date: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    finish_date: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    mou_file: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    customer_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "mou",
+    underscored: true,
+    timestamps: true,
+    getterMethods: {
+      file_url() {
+        return `https://api-simocvhp-production.up.railway.app/${this.mou_file}`;
+      },
+    },
+  }
 );
 
 mou.belongsTo(customer, {
-    foreignKey: "customer_id",
-    onDelete: "CASCADE",
-    onUpdate: "CASCADE",
+  foreignKey: "customer_id",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
 });
 customer.hasMany(mou, {
-    foreignKey: "customer_id",
-    onDelete: "CASCADE",
-    onUpdate: "CASCADE",
+  foreignKey: "customer_id",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
 });
 
 export default mou;
